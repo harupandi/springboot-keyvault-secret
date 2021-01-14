d30af69dd7a909a9b7c64da3da81ee069c8871d8
Bryan <bryan.pinedag@hotmail.com>
2021-01-13 19:28:42 -0600

    removed unnecesary import

--- src/main/java/com/example/keyvaultsecret/demo/Application.java
+++ src/main/java/com/example/keyvaultsecret/demo/Application.java
@@ -2,7 +2,6 @@ package com.example.keyvaultsecret.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.azure.identity.DefaultAzureCredentialBuilder;
